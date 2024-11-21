5111b510850de93b0f0cb38dd7c9d20853c2b7dd
Novik-XIV <glovesnovik@gmail.com>
2024-11-21 16:19:36 +0000

    Revert "nfc: nq-nci{pnt80t}: Disable hardware checks"
    This reverts commit f7b65e898f83ffba9cef85f0130b7514c787ac32.

--- drivers/nfc/nq-nci-pn80t.c
+++ drivers/nfc/nq-nci-pn80t.c
@@ -707,7 +707,6 @@ static const struct file_operations nfc_dev_fops = {
  * This function will block NFCC to enter FW download mode.
  */
 
-#if 0
 /* Check for availability of NQ_ NFC controller hardware */
 static int nfcc_hw_check(struct i2c_client *client, struct nqx_dev *nqx_dev)
 {
@@ -925,7 +924,6 @@ err_nfcc_hw_check:
 done:
 	return ret;
 }
-#endif
 
 /*
  * Routine to enable clock.
@@ -1242,7 +1240,6 @@ static int nqx_probe(struct i2c_client *client,
 	nqx_disable_irq(nqx_dev);
 
 	/* Do not perform nfcc_hw_check, make sure that nfcc is present */
-#if 0
 	/*
 	 * To be efficient we need to test whether nfcc hardware is physically
 	 * present before attempting further hardware initialisation.
@@ -1255,7 +1252,6 @@ static int nqx_probe(struct i2c_client *client,
 		/* We don't think there is hardware switch NFC OFF */
 		goto err_request_hw_check_failed;
 	}
-#endif
 
 	/* Register reboot notifier here */
 	r = register_reboot_notifier(&nfcc_notifier);
--- drivers/nfc/nq-nci.c
+++ drivers/nfc/nq-nci.c
@@ -936,12 +936,6 @@ err_nfcc_hw_info:
 }
 #endif
 
-/**
- * Do not need check availability of NFCC.
- * This function will block NFCC to enter FW download mode.
- */
-
-#if 0
 /* Check for availability of NQ_ NFC controller hardware */
 static int nfcc_hw_check(struct i2c_client *client, struct nqx_dev *nqx_dev)
 {
@@ -1133,7 +1127,6 @@ done:
 
 	return ret;
 }
-#endif
 
 /*
  * Routine to enable clock.
@@ -1467,8 +1460,6 @@ static int nqx_probe(struct i2c_client *client,
 	}
 	nqx_disable_irq(nqx_dev);
 
-	/* Do not perform nfcc_hw_check, make sure that nfcc is present */
-#if 0
 	/*
 	 * To be efficient we need to test whether nfcc hardware is physically
 	 * present before attempting further hardware initialisation.
@@ -1481,7 +1472,6 @@ static int nqx_probe(struct i2c_client *client,
 		/* We don't think there is hardware switch NFC OFF */
 		goto err_request_hw_check_failed;
 	}
-#endif
 
 	/* Register reboot notifier here */
 	r = register_reboot_notifier(&nfcc_notifier);
